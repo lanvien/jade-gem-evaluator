b6c05b08439a9c424bc261912e3ed378416b8e54
lanvien <lanvien.ha@gmail.com>
2026-08-23 17:32:57 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,1 @@
-import Header from "@/components/Header";
-import HeroSection from "@/components/HeroSection";
-import MissionSection from "@/components/MissionSection";
-import AppraisalSection from "@/components/AppraisalSection";
-import GuidesSection from "@/components/GuidesSection";
 
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-background">
-      <Header />
-      <HeroSection />
-      <MissionSection />
-      <AppraisalSection />
-      <GuidesSection />
-    </div>
-  );
-};
-
-export default Index;
